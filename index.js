17ed4179d47b72acc5f4bf9e6f06c9c0f733b4a9
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:15:43 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Code Along Norms: 
+// 1. Only be on Repl.it/classroom resources
+// 2. Actively follow along and engage
+// 3. Ask questions and flag if you get stuck
+// 4. Take notes as needed!
+let bffae= "Keke"
+console.log("My bffae name is" + bffae + ",an icon who produces music.");
+
